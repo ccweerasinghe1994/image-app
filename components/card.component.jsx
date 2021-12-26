e309c1a364b7d888dbcf449d2f9ae67ce4a66c2b
wchamara <chamara.weerasinghe@my.sliit.lk>
2021-12-26 22:37:28 +0530

    small user interface changes

--- components/card.component.jsx
+++ components/card.component.jsx
@@ -1,28 +1,37 @@
 import Image from "next/image";
 
-const Card = ()=>(
+const Card = ({tags="#test#test#test",description=" Lorem ipsum dolor sit, amet consectetur adipisicing elit.\n" +
+"                Repellat impedit",likes=32,userName="chamara",imageUrl="https://images.unsplash.com/photo-1640521515233-f1fa3f600e31?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"})=>(
     <div className="card">
         <div className="card__header">
             <div className="image-wrapper">
                 <Image  className="card__header-image" src={"/static/images/test-user.jpg"} width={40} height={40} alt={"profile image"} />
             </div>
-            <div className="card__header-name"><a>chamara</a></div>
+            <div className="card__header-name"><a>{userName}</a></div>
         </div>
         <div className="card__image">
-            <Image  src={"/static/images/baby.jpg"} width={640} height={960}  layout={"responsive"} alt={"card image"}/>
+            <Image  src={imageUrl} width={640} height={960}  layout={"responsive"} alt={"card image"}/>
         </div>
         <div className="card__footer">
             <div className="card__footer-likes">
-                <Image src={"/static/icons/like.svg"} height={20} width={20} alt={"like icons"}/>
-                32 likes</div>
+                <div className="card__footer-likes-icon">
+                    <Image src={"/static/icons/like.svg"} height={20} width={20} alt={"like icons"}/>
+                </div>
+                <div className="card__footer-likes-text">
+                    {likes} likes
+                </div>
+
+               </div>
             <p className="card__footer-text">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Repellat impedit
+                {description}
             </p>
-            <div className="card_footer-tags">
+            <div className="card__footer-tags">
                 <a href="#">
-                    #test#test#test
+                    {tags}
                 </a>
+            </div>
+            <div className="card__footer-comments">
+                view 12 comments
             </div>
         </div>
     </div>
