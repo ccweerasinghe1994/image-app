f83e64a116467b4688434722c157de6ee1b05f51
wchamara <chamara.weerasinghe@my.sliit.lk>
2021-12-26 17:33:19 +0530

    basic markup is finished

---
+++ components/card.component.jsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+const Card = ()=>(
+    <div className="card">
+        <div className="card__header">
+            <div className="image-wrapper">
+                <Image className="card__header-image" src={"/static/images/test-user.jpg"} width={40} height={40} alt={"profile image"} />
+            </div>
+            <div className="card__header-name">chamara</div>
+        </div>
+        <div className="card__image">
+            <Image src={"/static/images/baby.jpg"} width={640} height={960} layout={"responsive"} alt={"card image"}/>
+        </div>
+        <div className="card__footer">
+            <div className="card__footer-likes">
+                <Image src={"/static/icons/like.svg"} height={20} width={20} alt={"like icons"}/>
+                32 likes</div>
+            <p className="card__footer-text">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Repellat impedit
+            </p>
+            <div className="card_footer-tags">
+                <a href="#">
+                    #test#test#test
+                </a>
+            </div>
+        </div>
+    </div>
+
+)
+
+export default Card;
---
+++ components/nav-bar.jsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const NavBar = ()=>(
+    <nav className="navbar">
+        <div className="navbar-nav">
+            <div className="navbar-nav__link">
+                <Link href="/">
+                    <a>
+                        <Image src={"/static/icons/home.svg"} height={32} width={32} alt={"home icon"}/>
+                    </a>
+                </Link>
+            </div>
+            <div className="navbar-nav__link">
+                <Link href="/">
+                    <a>
+                        <Image src={"/static/icons/like.svg"} height={32} width={32} alt={"home icon"}/>
+                    </a>
+                </Link>
+            </div>
+        </div>
+
+
+    </nav>
+)
+
+export default NavBar;
