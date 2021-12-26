66518f74af5fb545114e1152725111c7f93175b8
wchamara <chamara.weerasinghe@my.sliit.lk>
2021-12-27 00:22:40 +0530

    adding default null props

--- components/card.component.jsx
+++ components/card.component.jsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
 
-const Card = ({profile_image,width,height,small,tags="#test#test#test",description,likes=32,username,imageUrl="https://images.unsplash.com/photo-1640521515233-f1fa3f600e31?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"})=>(
+const Card = ({profile_image,small,tags="#test#test#test",description,likes=32,username,imageUrl="https://images.unsplash.com/photo-1640521515233-f1fa3f600e31?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"})=>(
     <div className="card">
         <div className="card__header">
             <div className="image-wrapper">
-                <Image  className="card__header-image" src={profile_image.medium} width={40} height={40} alt={"profile image"} />
+                <Image  className="card__header-image" src={profile_image.medium ||null} width={40} height={40} alt={"profile image"} />
             </div>
-            <div className="card__header-name"><a>{username}</a></div>
+            <div className="card__header-name"><a>{username||null}</a></div>
         </div>
         <div className="card__image">
-            <Image  src={small} width={640} height={960}  layout={"responsive"} alt={"card image"}/>
+            <Image  src={small||null} width={640} height={960}  layout={"responsive"} alt={"card image"}/>
         </div>
         <div className="card__footer">
             <div className="card__footer-likes">
@@ -17,7 +17,7 @@ const Card = ({profile_image,width,height,small,tags="#test#test#test",descripti
                     <Image src={"/static/icons/like.svg"} height={20} width={20} alt={"like icons"}/>
                 </div>
                 <div className="card__footer-likes-text">
-                    {likes} likes
+                    {likes||null} likes
                 </div>
 
                </div>
@@ -27,7 +27,7 @@ const Card = ({profile_image,width,height,small,tags="#test#test#test",descripti
             </p>
             <div className="card__footer-tags">
                 <a href="#">
-                    {tags}
+                    {tags||null}
                 </a>
             </div>
             <div className="card__footer-comments">
