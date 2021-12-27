8eef452c318da551853541c5447aa0608947f383
wchamara <chamara.weerasinghe@my.sliit.lk>
2021-12-27 09:01:05 +0530

    added local fonts and _document.js

---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+import Document, {Head, Html, Main, NextScript} from "next/document";
+
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang={"eng"}>
+                <Head>
+
+                    <link rel={"preload"} href="fonts/Roboto-Light.ttf" as={"font"} crossOrigin={"anonymos"}></link>
+                    <link rel={"preload"} href="fonts/Lato-Thin.ttf" as={"font"} crossOrigin={"anonymos"}></link>
+                    <link rel={"preload"} href="fonts/BebasNeue-Regular.ttf" as={"font"} crossOrigin={"anonymos"}></link>
+                    {/*<link rel={"preload"} href="fonts/IBMPlexSans-Regular.ttf" as={"font"} crossOrigin={"anonymos"}></link>*/}
+                    {/*<link rel={"preload"} href="fonts/IBMPlexSans-Thin.ttf" as={"font"} crossOrigin={"anonymos"}></link>*/}
+                </Head>
+                <body>
+                <Main>
+                </Main>
+                <NextScript/>
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument;
